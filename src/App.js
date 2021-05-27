ce195241db188d603a081a1b2a162a4180879c9e
rashiop <red.alpacca@gmail.com>
2021-05-27 17:00:18 +0700

    bwa: handle offline

--- src/App.js
+++ src/App.js
@@ -7,9 +7,13 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Menu from './components/Menu';
+import Offline from './components/Offline';
 
 function App() {
   const [items, setItems] = useState([])
+  const [offlineStatus, setOfflineStatus] = useState(!navigator.onLine)
+
+  const handleOfflineStatus = () => setOfflineStatus(!navigator.onLine)
 
   useEffect(() => {
     (async function() {
@@ -23,10 +27,26 @@ function App() {
 
       const { nodes } = await response.json()
       setItems(nodes)
+
+      const script = document.createElement('script')
+      script.src = '/carousel.js';
+      script.async = false;
+      document.body.appendChild(script)
     })()
-  }, [])
+
+    handleOfflineStatus()
+    window.addEventListener('online', handleOfflineStatus)
+    window.addEventListener('offline', handleOfflineStatus)
+
+    return() => {
+      window.removeEventListener('online', handleOfflineStatus)
+      window.removeEventListener('offline', handleOfflineStatus)
+    }
+  }, [offlineStatus])
+
   return (
     <>
+    {offlineStatus && <Offline />}
     <Header />
     <Hero />
     <Browse />
---
+++ src/components/Offline.jsx
@@ -0,0 +1,20 @@
+const Offline = () => {
+  return (
+    <div className="bg-yellow-50 border-l-4 border-yellow-400 p-4">
+      <div className="flex ml-3">
+        <div className="flex-shrink-0">
+          <h2>
+            Ooops
+          </h2>
+          <div className="mt-3">
+            <div className="text-sm text-yellow-700">
+              You are offline
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Offline
